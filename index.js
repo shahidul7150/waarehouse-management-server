b9a7a40a3548a4701f0c0ae0e104c2f49d01df26
Shahidul Islam <shahidulislam7150@gmail.com>
2022-04-29 12:37:51 +0600

    add all

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +9,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmmj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        
+    }
+    finally{}
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('My f&c Stock server is ok' )
