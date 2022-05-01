12f9d6308850cb7b7000dec260197c6d9080a614
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-01 21:35:54 +0600

    create get api for all service

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,24 +9,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nmmj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-        
-    }
-    finally{}
-}
-run().catch(console.dir)
+  try {
+    await client.connect();
+    const serviceCollection = client.db("f2c-store").collection("service");
 
-app.get('/', (req, res) => {
-    res.send('My f&c Stock server is ok' )
-})
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("My f&c Stock server is ok");
+});
 
 app.listen(port, () => {
-    console.log("my f&C server running port",port);
-})
+  console.log("my f&C server running port", port);
+});
