b76c7ae64c06607236b6494631d6d64f1324571d
Shahidul Islam <shahidulislam7150@gmail.com>
2022-05-02 12:21:25 +0600

    create delete api

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
           const result = await serviceCollection.insertOne(newItem);
           res.send(result);
       })
+
+      app.delete('/service/:id', async (req, res) => {
+          const id = req.params.id;
+          const query = { _id: ObjectId(id) };
+          const result = await serviceCollection.deleteOne(query);
+          res.send(result);
+      })
   } finally {
   }
 }
