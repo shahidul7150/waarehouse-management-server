001d61b5ee393e72e07f4f4ce651b4262852cf58
unknown <shahidulislam7150@gmail.com>
2022-08-08 11:59:15 +0600

    server commit

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("My f&c Stock server is running");
+  res.send("My f&c Stock server is running...");
 });
 
 app.listen(port, () => {
